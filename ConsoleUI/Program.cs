33a8627af0a054f076f52c6a6f1c9470b1903890
pbansal16 <prateekbansalind@gmail.com>
2021-10-08 19:01:24 +1300

    C# Program Where I Use Common Variable Type To Store Data

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Prateek Bansal";
+            int age = 30;
+            bool isAlive = true;
+            string contactNumber = "+64XXXXXXXXXX";
+
+            Console.WriteLine($"{name}'s age is {age}.");
+            Console.WriteLine($"is he alive? {isAlive}");
+            Console.WriteLine($"His contact number is {contactNumber}");
+
+            Console.ReadLine();
+        }
+    }
+}
